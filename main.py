b65b4418db208079dee4379e87426e46a412c754
edumaciel45 <santosmacieleduardohenrique@gmail.com>
2026-01-20 16:19:15 -0300

    descricoes nas alteracoes

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env.python
+"Programa: hello word"
+
+def main():
+    print("hello word!")
+
+    if_name_== "-main-" : 
+    main()
+
+    print ("aprendendo git com python")
+
+    # main.py
+
+    print("ola mundo!")
+
+    
+
+
